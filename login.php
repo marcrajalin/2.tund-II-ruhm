17fb7f3d615659499885afe4f27c5be81a57e6a9
marcraja <marcraja@greeny.cs.tlu.ee>
2016-09-15 12:49:09 +0300

    added login page

--- login.php
+++ login.php
@@ -1,3 +1,27 @@
+<?php
+
+//var_dump($_GET);
+//echo "<br>";
+//var_dump($_POST);
+
+$signupEmailError = "";
+
+//kas on üldse olemas
+if (isset ($_POST["signupEmail"])) {
+	
+	//oli olemas, ehk keegi vajutas nuppu
+	//kas oli tühi
+	if (empty ($_POST["signupEmail"])) {
+		//oli töesti tühi
+		echo "email oli tühi";
+		$signupEmailError = "See väli on kohustuslik";
+	
+	}
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,18 +31,38 @@
 
  <h1>Logi sisse</h1>
  
- <form>
- 
- <input type="email">
+ <form method="POST">
+ <label>E-post</label>
+ <input name="loginEmail" type="email">
  
  <br><br>
+ <label>Password</label>
+ <input name="loginPassword" type="password">
  
- <input type="password">
  
  <br><br>
  
  <input type="submit">
  <form>
+ 
+ 
+ <h1>Logi kasutaja</h1>
+ 
+  <form method="POST">
+  
+ <label>E-post</label>
+ <input name="signupEmail" type="email"> <?php echo $signupEmailError; ?>
+ 
+ <br><br>
+ <label>Password</label>
+ <input name="loginPassword" type="password" 
+ 
+ 
+ <br><br>
+ 
+ <input type="submit" value="Loo kasutaja">
+ 
+ </form>
 
  
  </body>
